4aeb7550bc0358b052fc7891f58dda89c982fa94
tien22521469 <163420484+tien22521469@users.noreply.github.com>
2025-04-29 21:53:26 +0700

    Update BookWorkApplicationTests.java

--- emartapp/Backend/javaapi/src/test/java/com/springwork/bookwork/BookWorkApplicationTests.java
+++ emartapp/Backend/javaapi/src/test/java/com/springwork/bookwork/BookWorkApplicationTests.java
@@ -3,13 +3,16 @@ package com.springwork.bookwork;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.properties")
 class BookWorkApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test will verify that the Spring application context loads successfully
 	}
 
 }
